5ac3d56b70f02ac5b9c48ef91a5dfbad476ee263
Januarius-Njoku <76001800+Januarius-Njoku@users.noreply.github.com>
2022-08-01 16:49:57 +0100

    Added ParentComponent, _ChildComponent components, to demonstrating passing down params from parents to children, and attribute splatting.

---
+++ TangyModels/LearnBlazorModels/Demo_Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TangyModels.LearnBlazorModels
+{
+    public class Demo_Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public bool IsActive { get; set; }
+        public List<Demo_ProductProp> ProductProperties { get; set; }
+
+    }
+}
---
+++ TangyModels/LearnBlazorModels/Demo_ProductProp.cs
@@ -0,0 +1,9 @@
+﻿namespace TangyModels.LearnBlazorModels
+{
+    public class Demo_ProductProp
+    {
+        public int Id { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
