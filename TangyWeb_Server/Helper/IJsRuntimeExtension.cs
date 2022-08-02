d5cfbaa486e8b15bc8071ffd510664b51c17e015
Januarius-Njoku <76001800+Januarius-Njoku@users.noreply.github.com>
2022-08-02 11:16:01 +0100

    Integrated SweetAlert and ToastrJs using the IJsRuntimeExtension that contains JSRuntime extension methods.

---
+++ TangyWeb_Server/Helper/IJsRuntimeExtension.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.JSInterop;
+namespace TangyWeb_Server.Helper
+{
+    public static class IJsRuntimeExtension
+    {
+        public static async ValueTask ToastrSuccess(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+        }
+
+        public static async ValueTask ToastrError(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("ShowToastr", "error", message);
+        }
+        public static async ValueTask SweetAlertOk(this IJSRuntime jsRuntime, string title, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("Swal", "success", title, message);
+        }
+        public static async ValueTask SweetAlertCancel(this IJSRuntime jsRuntime, string title, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("Swal", "failure", title, message);
+        }
+
+
+    }
+}
---
+++ TangyWeb_Server/wwwroot/js/common.js
@@ -0,0 +1,17 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type === "success") {
+        toastr.success(message, "Operation Successful", { timeout: 5000 });
+    }
+    if (type === "error") {
+        toastr.success(message, "Operation Failed", { timeout: 5000 });
+    }
+};
+
+window.Swal = (type, title, text) => {
+    if (type === "success") {
+        swal(title, text, "success");
+    }
+    if (type === "failure") {
+        swal(title, text, "error");
+    }
+}
